e78a58f37bddc72da1d73e0e63bd1ce4c5e47390
Abishek6702 <abishekkrishnat@gmail.com>
2026-04-29 14:24:18 +0530

    cloudinary configuration and public url id for deletion event routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const upload = require("../middlewares/multerConfig.js");
+const upload = require("../middleware/multerConfig.js");
 
 const Event = require("../models/Event.js");
 const eventController = require("../controllers/eventController.js");
