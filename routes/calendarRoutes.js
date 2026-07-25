9a70aef9948811f53e116fb8fe65cc8cd07529c1
Abishek6702 <abishekkrishnat@gmail.com>
2026-07-25 11:07:46 +0530

    protect added for calendar

--- routes/calendarRoutes.js
+++ routes/calendarRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { getVenues, getEvents } = require("../controllers/calendarController");
+const protect = require("../middleware/protect")
 
 // GET /api/calendar/venues
-router.get("/venues", getVenues);
+router.get("/venues",protect, getVenues);
 
 // GET /api/calendar/events?venue=Main+Board+Room&view=week&date=2026-10-04
-router.get("/events", getEvents);
+router.get("/events",protect, getEvents);
 
 module.exports = router;
 
